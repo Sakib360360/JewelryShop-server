77f5b08bace53034ab1b13155d41147411fab87e
Sakib360360 <saakibabrar@gmail.com>
2023-10-09 08:57:08 +0600

    added

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+// username ----  Jewelry-Shop
+// pass --- TLrFpFL5cm6kjev6
+
+
+
+
+
+const uri = "mongodb+srv://Jewelry-Shop:TLrFpFL5cm6kjev6@cluster0.2utjecm.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // initialize
+        app.get('/', (req, res) => {
+            res.send('running')
+        })
+        app.listen(port, () => {
+            console.log('running')
+        })
+
+        const allJewelryCollections = client.db('Jewelry-Shop').collection('jewelries')
+        app.get('/alljwl', async (req, res) => {
+            const result = await allJewelryCollections.find().toArray()
+            res.send(result)
+            console.log(result)
+        })
+
+        // creating object
+        app.post('/addJwl', async (req, res) => {
+            const body = req.body;
+            const result = await allJewelryCollections.insertOne(body);
+            console.log(result)
+            res.send(result)
+        })
+        // data by user
+        app.get('/myJwl/:postedBy', async (req, res) => {
+            const postedBy = req.params.postedBy;
+            const result = await allJewelryCollections.find({ postedBy: postedBy }).toArray()
+            res.send(result)
+            console.log(result)
+        })
+        // delete item
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await allJewelryCollections.deleteOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+
+
+        
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
